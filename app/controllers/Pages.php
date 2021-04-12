dfc0358b73d8ed00a04e7b27b61e04154b9ecb90
KarlaYEP2 <karl-erik.voll@khk.ee>
2021-04-12 16:37:08 +0300

    Määratud ja võetud kasutusele kontrolleri meetod ning võmalikud parameetrid

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -11,4 +11,11 @@ class Pages
     {
         echo 'Pages class has been loaded<br>';
     }
+
+    public function index(){
+        echo 'index method has been loaded<br>';
+    }
+    public function about(){
+        echo 'about method has been loaded<br>';
+    }
 }
