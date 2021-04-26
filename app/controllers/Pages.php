f4d801064626125cdadc3db0e2a0f0d808c8a213
KarlaYEP2 <karl-erik.voll@khk.ee>
2021-04-26 11:28:02 +0300

    Rakenduse põhi - raamistik - on valmis

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -9,26 +9,15 @@ class Pages extends Controller
      */
     public function __construct()
     {
-      $this->pagesModel = $this->model('Page');
+        $this->pagesModel = $this->model('Page');
     }
 
-    public function index(){
-        //echo 'index method has been loaded<br>';
+    public function index()
+    {
         $data = array(
             'title' => 'THE PAGES PAGE :I',
             'content' => 'PAGES HAS BEEN LOADED LIKE ALWAYS BY MVC'
         );
         $this->view('pages/index', $data);
-    }
-    public function about(){
-        //model test
-        //$this->pagesModel->addUser();
-        // $users = $this->pagesModel->getUsers();
-        // print_r($users);
-        $usersCount = $this->pagesModel->usersCount();
-        echo 'Users in db - '.$usersCount.'<br>';
-        //echo 'about method has been loaded<br>';
-        $data = array('content' => 'PAGES ABOUT HAS BEEN LOADED BY MVC');
-        $this->view('pages/about', $data);
     }
 }
--- app/models/Page.php
+++ app/models/Page.php
@@ -12,25 +12,4 @@ class Page
     {
         $this->db = new Databases();
     }
-    public function addUser(){
-        $this->db->query('INSERT INTO users SET name=:name, email=:email, status=:status');
-        $this->db->bind('name', 'user2');
-        $this->db->bind('email', 'user2@localhost.loc');
-        $this->db->bind('status', 'admin');
-        $this->db->execute();
-    }
-    public function getOneUser(){
-        $this->db->query('SELECT * FROM users WHERE id=:id');
-        $this->db->bind('id', 1);
-        return $this->db->getOne();
-    }
-
-    public function getUsers(){
-        $this->db->query('SELECT * FROm users');
-        return $this->db->getAll();
-    }
-    public function usersCount(){
-        $this->getUsers();
-        return $this->db->rowCount();
-    }
 }
