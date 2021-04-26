9afa69b9f67b6c665764c31b8b84153369d956c8
KarlaYEP2 <karl-erik.voll@khk.ee>
2021-04-26 09:18:04 +0300

    LISATUD kasutusuele rakendusega seotud konstandid

---
+++ app/config/constants.php
@@ -0,0 +1,7 @@
+<?php
+// Project name
+define('PROJECT_NAME', 'tap_raamistik_ita19');
+// APp root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL root
+define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
